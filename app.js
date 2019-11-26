7ae9b975aed6315772b0e66dcd58cbfd468ca89a
Alson Shareef <alson.shareef@gmail.com>
2019-11-26 22:53:41 +0800

    set disk storage engine and initial upload

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const multer = require("multer");
 const ejs = require("ejs");
 const path = require("path");
 
+// Set Storage Engine
+const storage = multer.diskStorage({
+	destination: "./public/uploads/",
+	filename: (req, file, callback) => {
+		callback(
+			null,
+			file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+		);
+	}
+});
+
+// Upload
+const upload = multer({
+	storage: storage
+}).single("file");
+
 // INIT
 const app = express();
 const port = 3000;
@@ -15,7 +31,16 @@ app.get("/", (req, res) => res.render("index"));
 
 // POST
 app.post("/upload", (req, res) => {
-	res.send("POST request to /upload");
+	upload(req, res, err => {
+		if (err) {
+			res.render("index", {
+				msg: err
+			});
+		} else {
+			console.log(req.file);
+			res.send("test");
+		}
+	});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
